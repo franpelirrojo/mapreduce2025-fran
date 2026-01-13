96ebc7117769dade4ee8aad1c12669e3a37999c0
franpelirrojo <franfc.00@gmail.com>
2026-01-13 16:59:11 +0100

    ejercicio 5

--- exercises/exercise5/reducer.py
+++ exercises/exercise5/reducer.py
@@ -1,30 +1,15 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
 import sys
-
-maxItem = None
-maxTotal = 0.0
-salesTotal = 0.0
+salesTotal = 0
 oldKey = None
-
 for line in sys.stdin:
     data_mapped = line.strip().split("\t")
     if len(data_mapped) != 2: continue
     thisKey, thisSale = data_mapped
-
-    if oldKey is None: oldKey = thisKey
-
-    if oldKey != thisKey:
-        if salesTotal > maxTotal:
-            maxTotal = salesTotal
-            maxItem = oldKey
-        oldKey = thisKey
-        salesTotal = 0.0
-
+    if oldKey and oldKey != thisKey:
+        print(oldKey+"\t"+str(salesTotal))
+        oldKey = thisKey;
+        salesTotal = 0
+    oldKey = thisKey
     salesTotal += float(thisSale)
-
-if oldKey is not None and salesTotal > maxTotal:
-    maxTotal = salesTotal
-    maxItem = oldKey
-
-if maxItem is not None:
-    print(str(maxItem)+"\t"+str(maxTotal))
+if oldKey != None: print(oldKey+"\t"+str(salesTotal))
